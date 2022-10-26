828e33fe0faa7f401b443ef2e30e9a2eeeefbd48
Drfox81 <Drfox11181@mail.ru>
2022-10-26 16:04:32 +0300

    Готово

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Author first = new Author("Толстой", "Лев");
+        System.out.println("Автор первой книги - " + first.getNameAuthorFirst() + " " + first.getNameAuthorSecond());
+        Book bookOne = new Book(first, "Война и мир", 2010);
+        System.out.println("Год публикации - " + bookOne.getYearPublication());
+        bookOne.setYearPublication(2012);
+        System.out.println("Год публикации новый - " + bookOne.getYearPublication());
+        System.out.println("Фамилия автора - " + bookOne.getNameAuthor().getNameAuthorFirst());
+        System.out.println("Имя автора - " + bookOne.getNameAuthor().getNameAuthorSecond());
+        System.out.println();
+
+        Author second = new Author("Горький", "Максим");
+        System.out.println("Автор второй книги -  " + second.getNameAuthorFirst() + " " + second.getNameAuthorSecond());
+        Book bookTwo = new Book(second, "Вий", 1998);
     }
+
 }
